7ab6603288702a2d2450727134671e9dbbc83c16
Rogério Balloussier <143833175+RealRogerioSB@users.noreply.github.com>
2025-01-13 17:22:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,74 +1,30 @@
+import uvicorn
 from fastapi import FastAPI
-from redis_om import get_redis_connection, HashModel
+from fastapi.responses import HTMLResponse
+# from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-redis = get_redis_connection(
-    host="redis-11006.c250.eu-central-1-1.ec2.redns.redis-cloud.com",
-    port=11006,
-    decode_responses=True,
-    username="default",
-    password="oTAaMxsndx5SUHmKVZbTcT5uuhut3pss",
-)
+content_json = {
+    "1": ["Python", "Delphi"],
+    "2": ["JavaScript", "TypeScript"],
+    "3": ["Assembly", "Cobol"],
+}
 
-
-class Product(HashModel):
-    name: str
-    price: float
-    quantify: int
-
-    class Meta:
-        database = redis
-
-
-@app.get("/")
-def root():
-    return {"mensagem": "Aprendendo o FastAPI!"}
-
-
-@app.get("/help")
-def help():
-    return {"mensagem": "Aprendendo ainda esse FastAPI..."}
-
-
-from fastapi import FastAPI
-
-app = FastAPI()
+content_html = """
+<center>
+    <h1>FastAPI Web na <u>Geek University</u></h1>
+    <span>Para mais cursos, visite nosso site clicando <a href='https://www.geekuniversity.com.br' target='_blank'>
+    aqui</a>.</span>
+</center>
+"""
 
 
 @app.get("/")
 async def root():
-    return {"mensagem": "Página Raíz"}
+    return HTMLResponse(content=content_html)
+    # return JSONResponse(content=content_json)
 
 
-@app.get("/funci")
-async def funci():
-    return {
-        "cd_usu_fun": "F8719981",
-        "nm_usu_fun_abvd": "Rogério Balloussier",
-    }
-
-
-@app.post("/products")
-def create(product: Product):
-    return product.save()
-
-
-def redraw(pk: str):
-    product = Product.get(pk)
-    return {
-        "id": product.pk,
-        "name": product.name,
-        "price": product.price,
-        "quantify": product.quantify,
-    }
-
-
-@app.get("/products")
-def get():
-    return [redraw(pk) for pk in Product.all_pks()]
-
-
-@app.get("/products/{pk}")
-def get_product(pk: str):
-    return Product.get(pk)
+if __name__ == '__main__':
+    uvicorn.run("main:app", port=5000)
